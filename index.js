64e3cdffc8430fbc8922e377012e21eb114a1bc4
Abdullah Al Masoud <almasoud49@yahoo.com>
2024-09-30 17:20:10 +0600

    [DONE] url added

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000','https://baby-cares-store.vercel.app/products'],
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, 
+  allowedHeaders: 'Content-Type, Authorization',
+}));
 app.use(express.json());
 
 
